b957f71c6a1d15c027872946a6ba08bc995ced63
FireACNC <98172796+FireACNC@users.noreply.github.com>
2025-04-17 00:06:19 -0400

    up back

--- control.py
+++ control.py
@@ -638,6 +638,7 @@ class Control:
     
         front_left, front_right = front_pair = LegControl.TRIPOD_PAIRS[LegControl.FRONT]
         middle_pair = LegControl.TRIPOD_PAIRS[LegControl.MIDDLE]
+        back_pair = LegControl.TRIPOD_PAIRS[LegControl.BACK]
         # self.move_position(0, 0, 80)
         # time.sleep(delay)
 
@@ -648,18 +649,18 @@ class Control:
         self.lift_legs(front_pair)
         time.sleep(delay)
 
-        for _ in range(3):
-            self.stair_move(35, front_pair + middle_pair)
-        self.stair_move(0)
-        time.sleep(delay)
+        # for _ in range(3):
+        #     self.stair_move(35, front_pair + middle_pair)
+        # self.stair_move(0)
+        # time.sleep(delay)
 
         # roll, pitch, yaw = 0, -15, 0
         # points = self.calculate_posture_balance(roll, pitch, yaw)
         # self.transform_coordinates(points)
         # self.set_leg_angles()
 
-        # self.move_leg_positions(0, 0, 80, LegControl.TRIPOD_PAIRS[LegControl.BACK])
-        # time.sleep(delay)
+        self.move_leg_positions(0, 0, 80, back_pair)
+        time.sleep(delay)
 
         # restore z
         # also resetting lifted limbs
