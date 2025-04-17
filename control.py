8d2d0f5f6afdcea9d8029471f7b86d2444a23adc
FireACNC <98172796+FireACNC@users.noreply.github.com>
2025-04-16 22:28:50 -0400

    climb

--- control.py
+++ control.py
@@ -437,38 +437,30 @@ class Control:
     def climb_stair(self):
         delay = 0.5
     
-        # pair = LegControl.TRIPOD_PAIRS[LegControl.FRONT]
-        # # lower z to allow lifting up front legs
-        # self.move_position(0, 0, -15)
-        # time.sleep(delay)
+        front_pair = LegControl.TRIPOD_PAIRS[LegControl.FRONT]
+        # lower z to allow lifting up front legs
+        self.move_position(0, 0, 80)
+        time.sleep(delay)
 
-        # self.lift_legs(pair)
-        # time.sleep(delay)
+        self.lift_legs(front_pair)
+        time.sleep(delay)
 
-        # self.stair_move(35, pair)
-        # self.stair_move(35, pair)
-        # self.stair_move(0)
-        # time.sleep(delay)
+        self.stair_move(35, front_pair)
+        self.stair_move(35, front_pair)
+        self.stair_move(0)
+        time.sleep(delay)
 
-        # roll, pitch, yaw = 0, -15, 0
-        # points = self.calculate_posture_balance(roll, pitch, yaw)
-        # self.transform_coordinates(points)
-        # self.set_leg_angles()
+        roll, pitch, yaw = 0, -15, 0
+        points = self.calculate_posture_balance(roll, pitch, yaw)
+        self.transform_coordinates(points)
+        self.set_leg_angles()
 
-        # # restore z
-        # # also resetting lifted limbs
+        # restore z
+        # also resetting lifted limbs
+        time.sleep(delay * 2)
+
+        # self.move_leg_positions(0, 0, 80, LegControl.TRIPOD_PAIRS[LegControl.BACK])
         # time.sleep(delay * 2)
-
-        self.move_leg_positions(0, 0, -20, LegControl.TRIPOD_PAIRS[LegControl.BACK])
-        time.sleep(delay * 2)
-        self.move_leg_positions(0, 0, 20, LegControl.TRIPOD_PAIRS[LegControl.BACK])
-        time.sleep(delay * 2)
-        self.move_leg_positions(0, 0, 40, LegControl.TRIPOD_PAIRS[LegControl.BACK])
-        time.sleep(delay * 2)
-        self.move_leg_positions(0, 0, 60, LegControl.TRIPOD_PAIRS[LegControl.BACK])
-        time.sleep(delay * 2)
-        self.move_leg_positions(0, 0, 80, LegControl.TRIPOD_PAIRS[LegControl.BACK])
-        time.sleep(delay * 2)
 
         self.move_position(0, 0, 0)
 
