1fea9d47e893cdc51104d0cc6858ac4d7e459b3c
FireACNC <98172796+FireACNC@users.noreply.github.com>
2025-04-29 11:01:35 -0400

    control cont

--- control.py
+++ control.py
@@ -683,11 +683,14 @@ class Control:
         self.move_leg_positions(0, -60, -40, [front_pair[0]])
         self.move_leg_positions(0, 0, 40, [front_pair[0]])
         self.move_leg_positions(0, -60, -40, [back_pair[0]])
-        self.move_leg_positions(0, 0, 50, [back_pair[0]])
+        self.move_leg_positions(0, 0, 60, [back_pair[0]])
         self.move_leg_positions(0, -60, -40, [front_pair[1]])
         self.move_leg_positions(0, 0, 40, [front_pair[1]])
         self.move_leg_positions(0, -60, -40, [back_pair[1]])
-        self.move_leg_positions(0, 0, 50, [back_pair[1]])
+        self.move_leg_positions(0, 0, 60, [back_pair[1]])
+        self.move_leg_positions(0, 60, 0, back_pair)
+        self.move_leg_positions(0, 60, 0, front_pair)
+
 
 
         # self.move_leg_positions(0, -30, 0, middle_pair)
