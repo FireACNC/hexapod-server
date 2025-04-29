a76bb897ad745afc94b5b0c337079fc03b40204c
FireACNC <98172796+FireACNC@users.noreply.github.com>
2025-04-29 11:13:37 -0400

    a big hop

--- control.py
+++ control.py
@@ -674,12 +674,13 @@ class Control:
         self.move_leg_positions(0, -60, -40, [back_pair[1]])
         self.move_leg_positions(0, 0, 60, [back_pair[1]])
         self.move_leg_positions(0, 0, 20, back_pair)
-        self.move_leg_positions(0, 30, 0, back_pair)
         self.move_leg_positions(0, 30, 50, front_pair)
         self.move_leg_positions(0, 0, 50, front_pair)
         self.move_leg_positions(0, 0, 50, front_pair)
 
+        self.move_leg_positions(0, 30, 0, back_pair)
         self.move_leg_positions(0, 0, -200, middle_pair)
+        self.move_leg_positions(0, 0, 100, back_pair)
         # self.move_leg_positions(0, 40, 0, middle_pair)
 
         # self.move_leg_positions(0, -60, -40, [front_pair[0]])
